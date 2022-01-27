e48c9430d169f243e0fd66bd0e92f23b29a98943
BernatLl <blloret@hotmail.com>
2022-01-27 17:15:23 +0000

    er

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,16 +1,16 @@
 import React, { useState } from "react";
 import "../../styles/index.css";
 //create your first component
-const Home = () => {
-	const [onColor, setonColor] = useState("red");
+const TrafficLigth = () => {
+	const [color, setcolor] = useState("red");
 
 	return (
 		<div className="trafficlight">
-			<div className="light red switchOn"></div>
+			<div className="light red on"></div>
 			<div className="light yellow"></div>
 			<div className="light green"></div>
 		</div>
 	);
 };
 
-export default Home;
+export default TrafficLigth;
---
+++ src/js/views/home.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "../../styles/index.css";
+import TrafficLigth from "../component/TrafficLight.jsx";
+//create your first component
+const Home = () => {
+	return (
+		<>
+			<TrafficLigth />
+		</>
+	);
+};
+
+export default Home;
