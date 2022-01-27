1b4343846893271db913c22bfc7a4ec04af8a574
BernatLl <blloret@hotmail.com>
2022-01-27 17:44:14 +0000

    run

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -2,13 +2,25 @@ import React, { useState } from "react";
 import "../../styles/index.css";
 //create your first component
 const TrafficLigth = () => {
-	const [color, setcolor] = useState("red");
+	const [color, setcolor] = useState("red:'', yellow:'', green:''");
 
 	return (
 		<div className="trafficlight">
-			<div className="light red on"></div>
-			<div className="light yellow"></div>
-			<div className="light green"></div>
+			<div
+				className={`light red ${color.red}`}
+				onClick={() =>
+					setcolor({ red: "on", yellow: "", green: "" })
+				}></div>
+			<div
+				className={`light yellow ${color.yellow}`}
+				onClick={() =>
+					setcolor({ red: "", yellow: "on", green: "" })
+				}></div>
+			<div
+				className={`light green ${color.green}`}
+				onClick={() =>
+					setcolor({ red: "", yellow: "", green: "on" })
+				}></div>
 		</div>
 	);
 };
